be5f63c6afef6423bae9996ab48425b80a1ceec3
Mikola1918 <59391486+Mikola1918@users.noreply.github.com>
2020-01-04 00:38:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function download() {
+    var link = document.createElement('a');
+link.setAttribute('href','<H:/Wallpaper site/localhost/pic/cosmos14.jpg>');
+link.setAttribute('download','download');
+onload=link.click();
+}
